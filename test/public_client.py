6441953f385061b8044e8aea781d0e674b67346f
Kamil Madac <kamil.madac@gmail.com>
2013-11-11 23:56:15 +0100

    Issue #3 public part compatible with python 3

--- test/public_client.py
+++ test/public_client.py
@@ -10,27 +10,27 @@ class bitstamp_public_TestCase(unittest.TestCase):
 
     def test_ticker(self):
         ticker = self.client.ticker()
-        print ticker
+        print(ticker)
         self.assertIsInstance(ticker, dict)
 
     def test_order_book(self):
         order_book = self.client.order_book(group=True)
-        print order_book
+        print(order_book)
         self.assertIsInstance(order_book, dict)
 
     def test_transactions(self):
         transactions = self.client.transactions()
-        print transactions
+        print(transactions)
         self.assertIsInstance(transactions, list)
 
     def test_bitinstant_reserves(self):
         bitinstant_reserves = self.client.bitinstant_reserves()
-        print bitinstant_reserves
+        print(bitinstant_reserves)
         self.assertIsInstance(bitinstant_reserves, dict)
 
     def test_conversion_rate_usd_eur(self):
         conversion_rate_usd_eur = self.client.conversion_rate_usd_eur()
-        print conversion_rate_usd_eur
+        print (conversion_rate_usd_eur)
         self.assertIsInstance(conversion_rate_usd_eur, dict)
 
 if __name__ == '__main__':
