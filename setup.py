42be88b17002d3b400a91943f504dd6a00ff6f5d
Kamil Madac <kamil.madac@gmail.com>
2014-04-27 22:43:12 +0200

    Issue #16 - changed version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ class Tox(TestCommand):
 
 setup(
     name='BitstampClient',
-    version='2.0.2',
+    version='2.0.3',
     description='Bitstamp API python implementation',
     packages=['bitstamp'],
     url='',
