f278468e0e2800ffa7f420a0b7aff281d5e5cebf
Kamil Madac <kamil.madac@gmail.com>
2019-09-13 22:36:27 +0200

    Bump version to 2.2.8

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ class Tox(TestCommand):
 
 setup(
     name='BitstampClient',
-    version='2.2.7',
+    version='2.2.8',
     description='Bitstamp API python implementation',
     packages=['bitstamp'],
     url='https://github.com/kmadac/bitstamp-python-client',
