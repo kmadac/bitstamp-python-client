0f1da3db8382e0e1c91ed9b1d6d2d7180edbb29f
Kamil Madac <kamil.madac@gmail.com>
2022-10-26 19:40:08 +0200

    Added CI Testing on python 3.8, removed 3.4 and 3.5

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ class Tox(TestCommand):
 
 setup(
     name='BitstampClient',
-    version='2.2.8',
+    version='2.2.9',
     description='Bitstamp API python implementation',
     packages=['bitstamp'],
     url='https://github.com/kmadac/bitstamp-python-client',
